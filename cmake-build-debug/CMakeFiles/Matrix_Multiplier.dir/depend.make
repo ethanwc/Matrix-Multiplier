a1d113c2982c0d2a04b5d086f923d68c8e65df59
ethanwc <ethanwc@u.washington.edu>
2019-05-20 12:56:19 -0700

    added put and get

---
+++ cmake-build-debug/CMakeFiles/Matrix_Multiplier.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Matrix_Multiplier.
+# This may be replaced when dependencies are built.
---
+++ prodcons.c
@@ -0,0 +1,60 @@
+/*
+ *  prodcons module
+ *  Producer Consumer module
+ *
+ *  Implements routines for the producer consumer module based on
+ *  chapter 30, section 2 of Operating Systems: Three Easy Pieces
+ *
+ *  University of Washington, Tacoma
+ *  TCSS 422 - Operating Systems
+ *  Spring 2019
+ */
+
+// Include only libraries for this module
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include "counter.h"
+#include "matrix.h"
+#include "pcmatrix.h"
+#include "prodcons.h"
+
+
+// Define Locks and Condition variables here
+
+// Producer consumer data structures
+
+// Bounded buffer bigmatrix defined in prodcons.h
+//Matrix ** bigmatrix;
+
+
+int buffer[MAX];
+int fill_ptr = 0;
+int use_ptr = 0;
+int count = 0;
+
+void put(int value) {
+    buffer[fill_ptr] = value;
+    fill_ptr = (fill_ptr + 1) % MAX;
+    count++;
+}
+
+int get() {
+    int tmp = buffer[use_ptr];
+    use_ptr = (use_ptr + 1) % MAX;
+    count--;
+    return tmp;
+}
+
+
+// Matrix PRODUCER worker thread
+void *prod_worker(void *arg)
+{
+  return NULL;
+}
+
+// Matrix CONSUMER worker thread
+void *cons_worker(void *arg)
+{
+  return NULL;
+}
