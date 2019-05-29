ea5bfd064b5a80d3f943bfede2ed43af4aba8748
ethanwc <ethanwc@u.washington.edu>
2019-05-29 12:38:49 -0700

    update

---
+++ cmake-build-debug/CMakeFiles/matrix.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/ethanwc/Documents/380/Matrix-Multiplier222/counter.c" "/home/ethanwc/Documents/380/Matrix-Multiplier222/cmake-build-debug/CMakeFiles/matrix.dir/counter.c.o"
+  "/home/ethanwc/Documents/380/Matrix-Multiplier222/matrix.c" "/home/ethanwc/Documents/380/Matrix-Multiplier222/cmake-build-debug/CMakeFiles/matrix.dir/matrix.c.o"
+  "/home/ethanwc/Documents/380/Matrix-Multiplier222/pcmatrix.c" "/home/ethanwc/Documents/380/Matrix-Multiplier222/cmake-build-debug/CMakeFiles/matrix.dir/pcmatrix.c.o"
+  "/home/ethanwc/Documents/380/Matrix-Multiplier222/prodcons.c" "/home/ethanwc/Documents/380/Matrix-Multiplier222/cmake-build-debug/CMakeFiles/matrix.dir/prodcons.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/matrix.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/matrix.dir/pcmatrix.c.o"
+  "CMakeFiles/matrix.dir/matrix.c.o"
+  "CMakeFiles/matrix.dir/counter.c.o"
+  "CMakeFiles/matrix.dir/prodcons.c.o"
+  "matrix.pdb"
+  "matrix"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/matrix.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
